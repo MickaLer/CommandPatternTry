cb11c6c4b6bdb1ac5b944a0178687e5a769fc7b6
Micka <>
2025-12-11 10:18:24 +0100

    Project Commit

---
+++ Assets/Scripts/Commands/InteractCommand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class InteractCommand : Command
+{
+    private PickUpCommand _banane;
+    public override void Do(Cube cube)
+    {
+        if (cube.InteractWithBanana && cube.BananaObject != null)
+        {
+            _banane = new PickUpCommand(cube.BananaObject);
+            _banane.Do(cube);
+            cube.InteractWithBanana = false;
+        }
+    }
+
+    public override void Undo(Cube cube)
+    {
+        if (_banane != null)
+        {
+            _banane.Undo(cube);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CommanPatternTry
+My try on the Command Pattern.
+Use arrow keys for movements.
+A for color change
+E to put blocks
+R to interact
+Y to get a random number
+Left Click to shoot a ball
+
+Space to undo commands
